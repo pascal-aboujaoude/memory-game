3ae0f760273ea2ee972bbc78a06db6e3b3ea1e43
Pascal Abou Jaoude <pascal.aboujaoude@gmail.com>
2022-05-21 13:18:47 +0300

    adding mediaquery

---
+++ game/game.js
@@ -0,0 +1,13 @@
+const init = () => {
+  const soundGreen = new audio();
+  const soundRed = new audio();
+  const soundYellow = new audio();
+  const soundBlue = new audio();
+  const wrong = new audio();
+
+  new.addEventListener("click", (e) => {
+    
+  });
+
+  init();
+};
