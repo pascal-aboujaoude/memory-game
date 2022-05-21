1a0c9fe3e7d6dff9983b893567367d746feafbe5
Pascal Abou Jaoude <pascal.aboujaoude@gmail.com>
2022-05-21 21:49:47 +0300

    adding audio in js

--- game/game.js
+++ game/game.js
@@ -1,13 +1,24 @@
-const init = () => {
-  const soundGreen = new audio();
-  const soundRed = new audio();
-  const soundYellow = new audio();
-  const soundBlue = new audio();
-  const wrong = new audio();
+function gameOver() {
+  var wrong = new Audio("assets/wrong.mp3");
+  wrong.play();
+}
 
-  new.addEventListener("click", (e) => {
-    
-  });
+document.getElementById("green").addEventListener("click", function () {
+  var green = new Audio("assets/green.mp3");
+  green.play();
+});
 
-  init();
-};
+document.getElementById("red").addEventListener("click", function () {
+  var red = new Audio("assets/red.mp3");
+  red.play();
+});
+
+document.getElementById("blue").addEventListener("click", function () {
+  var blue = new Audio("assets/blue.mp3");
+  blue.play();
+});
+
+document.getElementById("yellow").addEventListener("click", function () {
+  var yellow = new Audio("assets/yellow.mp3");
+  yellow.play();
+});
