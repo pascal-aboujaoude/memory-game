18c4ae92aa2bfbc4864d5f943710ea8cce37fce0
Pascal Abou Jaoude <pascal.aboujaoude@gmail.com>
2022-05-22 21:07:57 +0300

    fixing html + css

--- game/game.js
+++ game/game.js
@@ -1,10 +1,16 @@
-function gameOver() {
-  var wrong = new Audio("assets/wrong.mp3");
-  wrong.play();
-}
+var level = level + 1;
+var started = 0;
+var lost = 0;
+
+// function gameOver() {
+//   var wrong = new Audio("assets/wrong.mp3");
+//   wrong.play();
+// }
 
 document.getElementById("green").addEventListener("click", function () {
   var green = new Audio("assets/green.mp3");
+  var greenDiv = document.getElementById("green");
+  greenDiv.style.boxShadow = "10px 20px 30px black";
   green.play();
 });
 
@@ -22,3 +28,37 @@ document.getElementById("yellow").addEventListener("click", function () {
   var yellow = new Audio("assets/yellow.mp3");
   yellow.play();
 });
+
+function sounds() {
+  var rand = [green, red, blue, yellow];
+  var randSound = rand[Math.floor(Math.random() * rand.length)];
+  return randSound;
+}
+
+function restartGame() {
+  started = 1;
+}
+
+function gameOver() {
+  started = 0;
+  lost = 1;
+  var wrong = new Audio("assets/wrong.mp3");
+  wrong.play();
+  var title = document.getElementByClassName("title-game");
+  title.innerHTML = "YOU LOST, RESTART GAME";
+}
+
+var start = document.getElementById("start");
+start.addEventListener("click", function () {
+  var soundToPlay = sounds();
+  var audio = new Audio("assets/+${soundToPlay}.mp3");
+  audio.play();
+});
+
+// for (var x = 0; x < start.length; x++) {
+//   if (x === randSound) {
+//     title.innerHTML = "LEVEL" + level;
+//   } else {
+//     gameOver();
+//   }
+// }
