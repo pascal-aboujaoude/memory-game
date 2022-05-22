4090c8c1040ec970404d5abb7db9a9f419eddd43
Pascal Abou Jaoude <pascal.aboujaoude@gmail.com>
2022-05-22 23:29:59 +0300

    adding nextSequence + conditions to colors

--- game/game.js
+++ game/game.js
@@ -13,42 +13,83 @@ document.body.addEventListener("click", function () {
   }
 });
 
-document.getElementById("green").addEventListener("click", function () {
-  var audio = new Audio("sounds/green.mp3");
-  audio.play();
-  document.getElementById("green").classList.add("pressed");
-  userClickedPattern.push("green");
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  text.innerHTML = "Level " + level;
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+  document.getElementById(randomChosenColour).classList.add("pressed");
   setTimeout(() => {
-    document.getElementById("green").classList.remove("pressed");
+    document.getElementById(randomChosenColour).classList.remove("pressed");
   }, 100);
+}
+
+document.getElementById("green").addEventListener("click", function () {
+  if (started) {
+    var audio = new Audio("assets/green.mp3");
+    audio.play();
+    document.getElementById("green").classList.add("pressed");
+    userClickedPattern.push("green");
+    checkAnswer(userClickedPattern.length - 1);
+    setTimeout(() => {
+      document.getElementById("green").classList.remove("pressed");
+    }, 100);
+  } else {
+    text.innerHTML = "Level" + level;
+    nextSequence();
+    started = true;
+  }
 });
 
 document.getElementById("blue").addEventListener("click", function () {
-  var audio = new Audio("sounds/blue.mp3");
-  audio.play();
-  document.getElementById("blue").classList.add("pressed");
-  userClickedPattern.push("blue");
-  setTimeout(() => {
-    document.getElementById("blue").classList.remove("pressed");
-  }, 100);
+  if (started) {
+    var audio = new Audio("assets/blue.mp3");
+    audio.play();
+    document.getElementById("blue").classList.add("pressed");
+    userClickedPattern.push("blue");
+    checkAnswer(userClickedPattern.length - 1);
+    setTimeout(() => {
+      document.getElementById("blue").classList.remove("pressed");
+    }, 100);
+  } else {
+    text.innerHTML = "Level" + level;
+    nextSequence();
+    started = true;
+  }
 });
 
 document.getElementById("yellow").addEventListener("click", function () {
-  var audio = new Audio("sounds/yellow.mp3");
-  audio.play();
-  document.getElementById("yellow").classList.add("pressed");
-  userClickedPattern.push("yellow");
-  setTimeout(() => {
-    document.getElementById("yellow").classList.remove("pressed");
-  }, 100);
+  if (started) {
+    var audio = new Audio("assets/yellow.mp3");
+    audio.play();
+    document.getElementById("yellow").classList.add("pressed");
+    userClickedPattern.push("yellow");
+    checkAnswer(userClickedPattern.length - 1);
+    setTimeout(() => {
+      document.getElementById("yellow").classList.remove("pressed");
+    }, 100);
+  } else {
+    text.innerHTML = "Level" + level;
+    nextSequence();
+    started = true;
+  }
 });
 
 document.getElementById("red").addEventListener("click", function () {
-  var audio = new Audio("sounds/red.mp3");
-  audio.play();
-  document.getElementById("red").classList.add("pressed");
-  userClickedPattern.push("red");
-  setTimeout(() => {
-    document.getElementById("red").classList.remove("pressed");
-  }, 100);
+  if (started) {
+    var audio = new Audio("assets/red.mp3");
+    audio.play();
+    document.getElementById("red").classList.add("pressed");
+    userClickedPattern.push("red");
+    checkAnswer(userClickedPattern.length - 1);
+    setTimeout(() => {
+      document.getElementById("red").classList.remove("pressed");
+    }, 100);
+  } else {
+    text.innerHTML = "Level" + level;
+    nextSequence();
+    started = true;
+  }
 });
